be62b8ff428169191db10ba6c7a9395bf177675d
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-03-06 18:07:48 +0100

    Cards

--- examen-sandra-cabrera/src/views/Home.vue
+++ examen-sandra-cabrera/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <Header/>
-    <Footer/>
+    <Cards/>
+    <Footer class="footer"/>
   </div>
 </template>
 
 <script>
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Cards from '../components/Cards'
 
 export default {
     components: {
       Header,
       Footer,
+      Cards
     }
 };
 </script>
