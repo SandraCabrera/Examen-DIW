5524170eb8e2a12f9da102e1d32b94d941cd63fc
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-03-06 17:04:33 +0100

    Footer

---
+++ examen-sandra-cabrera/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+    <footer>
+        <p>
+            © 2020 FIDE International Chess Federation. All Rights Reserved. No part of this site may be reproduced, stored in a retrieval system or transmitted in any way or by any means(including photocopying, recording or storing it in any medium by electronic means), without the written permission of FIDE International Chess Federation.
+        </p>
+    </footer>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+$color_fondo: #334166;
+$color_letras: #687A88;
+
+footer {
+    background-color: $color_fondo;
+
+    p {
+        color: #687A88;
+        text-align:center;
+    }
+}
+</style>
--- examen-sandra-cabrera/src/views/Home.vue
+++ examen-sandra-cabrera/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <Header/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default {
     components: {
       Header,
+      Footer,
     }
 };
 </script>
